d8e7f37ca6fb5cdd544b6b6f16b2a3d2ddcb60fe
priti singh <Singhpriti281@gmail.com>
2024-02-08 17:21:21 +0530

    Roll it

--- SimpleDice/ViewController.swift
+++ SimpleDice/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+  
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceViewOptionOne: UIImageView!
+    @IBOutlet weak var diceViewOptionTwo: UIImageView!
+   
+
+    @IBAction func rollTheDice(_ sender: UIButton) {
+        var diceOptions = [ #imageLiteral(resourceName: "DiceOne") , #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "dice6") ]
+        
+        diceViewOptionOne.image = diceOptions[Int.random(in: 0...5)]
+        diceViewOptionTwo.image = diceOptions[Int.random(in: 0...5)]
     }
-
-
+    
 }
 
