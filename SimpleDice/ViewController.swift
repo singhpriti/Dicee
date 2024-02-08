07a5c7c1df51aba752a88111a413ee29c8a82ffb
priti singh <Singhpriti281@gmail.com>
2024-02-08 12:55:03 +0530

    Initial Commit

---
+++ SimpleDice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleDice
+//
+//  Created by Preity Singh on 08/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
